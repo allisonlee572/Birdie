a6a5836620d723b5386c7e3fce7447fde5046756
allisonlee <allison.682.lee@gmail.com>
2021-10-02 13:42:45 -0700

    added codes

---
+++ src/battle_enemy1.py
@@ -0,0 +1,76 @@
+from pygame.sprite import Sprite
+import pygame
+import random as r
+from config import *
+
+
+class BattleEnemy1(Sprite):
+
+    def __init__(self, x, y, direction, size, screen):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.screen = screen
+        self.direction = direction
+        self.size = size
+        self.speed = 6
+        self.player_float_timer = PLAYER_FLOAT_DELAY
+
+        self.right_images = []
+        self.left_images = []
+        for idx in range(1,2):
+            self.image = pygame.image.load(f'assets/Avatar_Boli_front.png')
+            self.image = pygame.transform.scale(self.image, (int(size * 2), (int(size * 2))))
+            self.right_images.append(self.image)
+
+            # self.screen.blit(self.image, (self.x, self.y))
+
+            left_image = pygame.transform.flip(self.image, True, False)
+            self.left_images.append(left_image)
+
+            self.image_index = 0
+
+            # self.player_flap_timer = PLAYER_FLAP_DELAY
+
+            if self.direction == RIGHT:
+                self.images = self.right_images
+            if self.direction == LEFT:
+                self.images = self.left_images
+
+            self.image = self.get_next_image()
+            self.rect = pygame.Rect(x, y, self.image.get_width(), self.image.get_height())
+
+
+        # self.images = []
+
+        # self.images.append(img)
+        # self.image_index = 0
+        # image = self.images[0]
+
+    def get_next_image(self):
+        self.player_float_timer -= 1
+        if self.player_float_timer == 0:
+            self.player_float_timer = PLAYER_FLOAT_DELAY
+            self.image_index += 1
+        if self.image_index == len(self.images):
+            self.image_index = 0
+        if self.direction == RIGHT:
+            self.images = self.right_images
+        if self.direction == LEFT:
+            self.images = self.left_images
+        return self.images[self.image_index]
+
+    def update(self):
+        # self.screen.blit(self.image, (self.x, self.y))
+
+        self.image = self.get_next_image()
+        
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+        # image = self.get_next_image()
+
+        if DEBUG_MODE:
+            pygame.draw.rect(self.screen, BLUE, self.rect)
+        self.screen.blit(self.image, (self.x, self.y))
+
--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ import random as r
 from player import Player
 from enemy1 import Enemy1
 from enemy2 import Enemy2
+from battle_player import BattlePlayer
+from battle_enemy1 import BattleEnemy1
 from config import *
 
 
@@ -32,7 +34,17 @@ class Puddles:
 
         self.enemy1_group = Group()
         self.enemy1 = Enemy1(200, 330, RIGHT, SPRITE_SIZE, self.screen)
+        self.enemy1_group.add(self.enemy1)
+
+        self.enemy2_group = Group()
         self.enemy2 = Enemy2(400,330, RIGHT, SPRITE_SIZE, self.screen)
+        self.enemy2_group.add(self.enemy2)
+
+        self.battle_player_group = Group()
+        self.battle_player = BattlePlayer(WIDTH/2, 330, RIGHT, SPRITE_SIZE, self.screen)
+
+        self.battle_enemy1_group = Group()
+        self.battle_enemy1 = BattleEnemy1(WIDTH/2, 0, RIGHT, SPRITE_SIZE, self.screen)
 
         self.mode = GAME_STARTED
 
@@ -52,6 +64,16 @@ class Puddles:
     def handle_player_enemy1_collision(self, player, enemy1):
         if player.rect.colliderect(enemy1.rect):
             self.background = pygame.image.load('assets/bg02.png')
+            self.battle_player_group.add(self.battle_player)
+            self.battle_enemy1_group.add(self.battle_enemy1)
+            return True
+        else:
+            return False
+
+    def handle_player_enemy2_collision(self, player, enemy2):
+        if player.rect.colliderect(enemy2.rect):
+            self.background = pygame.image.load('assets/bg03.png')
+            print("collision correct")
             return True
         else:
             return False
@@ -85,10 +107,15 @@ class Puddles:
 
     def handle_game_in_session(self):
         pygame.sprite.groupcollide(self.player_group, self.enemy1_group, True, True, self.handle_player_enemy1_collision)
+        pygame.sprite.groupcollide(self.player_group, self.enemy2_group, True, True, self.handle_player_enemy2_collision)
+
 
         self.player_group.update()
         self.enemy1.update()
         self.enemy2.update()
+
+        self.battle_player_group.update()
+        self.battle_enemy1_group.update()
 
 
 if __name__ == '__main__':
